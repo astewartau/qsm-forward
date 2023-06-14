37ff803d877a49ca0ce9f7b7d04ba983e1de9258
Ashley Stewart <a.stewart.au@gmail.com>
2023-06-14 15:36:07 +1000

    Added generate_bids

--- qsm_forward/examples/main.py
+++ qsm_forward/examples/main.py
@@ -1,131 +1,13 @@
-import glob
-
 import json
 import os
 import nibabel as nib
 import numpy as np
-
 import qsm_forward
 
-tissue_params = {
-    "chi_path" : "head-phantom-maps/ChiModelMIX_noCalc.nii.gz",
-    "M0_path" : "head-phantom-maps/M0.nii.gz",
-    "R1_path" : "head-phantom-maps/R1.nii.gz",
-    "R2star_path" : "head-phantom-maps/R2star.nii.gz",
-    "mask_path" : "head-phantom-maps/BrainMask.nii.gz",
-    "seg_path" : "head-phantom-maps/SegmentedModel.nii.gz"
-}
-
-recon_params = {
-    "Subject" : "1",
-    "Session" : "1",
-    "Run" : 1,
-    "TR" : 50e-3,
-    "TEs" : np.array([ 4e-3, 12e-3, 20e-3, 28e-3 ]),
-    "flip_angle" : 15,
-    "B0" : 7,
-    "B0_dir" : np.array([0, 0, 1]),
-    "phase_offset" : 0,
-    "shimm" : False,
-    "voxel_size" : np.array([1.0, 1.0, 1.0]),
-    "peak_snr" : np.inf,
-    "out_dir" : os.path.join(os.getcwd(), "bids")
-}
-
-
 if __name__ == "__main__":
-    # create output directories
-    print("Creating output directory...")
-    os.makedirs(recon_params['out_dir'], exist_ok=True)
-    
-    # recon name
-    recon_name = f"sub-{recon_params['Subject']}_ses-{recon_params['Session']}_run-{recon_params['Run']}"
-    session_dir = os.path.join(recon_params['out_dir'], f"sub-{recon_params['Subject']}", f"ses-{recon_params['Session']}")
-    os.makedirs(os.path.join(session_dir, 'anat'))
-    os.makedirs(os.path.join(session_dir, 'extra_data'))
+    bids_dir = "bids"
+    tissue_params = qsm_forward.default_tissue_params.copy()
+    recon_params = qsm_forward.default_recon_params.copy()
 
-    # calculate field
-    print("Computing field model...")
-    chi_nii = nib.load(tissue_params['chi_path'])
-    chi = chi_nii.get_fdata()
-    field = qsm_forward.generate_field(chi)
-    del chi
-
-    # simulate shim field
-    print("Computing shim fields...")
-    brain_mask = nib.load(tissue_params['mask_path']).get_fdata()
-    _, field, _ = qsm_forward.generate_shimmed_field(field, brain_mask, order=2)
-
-    # phase offset
-    print("Computing phase offset...")
-    M0 = nib.load(tissue_params['M0_path']).get_fdata()
-    phase_offset = qsm_forward.generate_phase_offset(M0, brain_mask, M0.shape)
-    del brain_mask
-
-    # image-space resizing of chi
-    print("Image-space resizing of chi...")
-    chi_downsampled_nii = qsm_forward.resize(chi_nii, recon_params['voxel_size'])
-    nib.save(chi_downsampled_nii, filename=os.path.join(session_dir, "extra_data", "chi.nii"))
-    print("Image-space cropping of brain mask...")
-    mask_downsampled_nii = qsm_forward.resize(nib.load(tissue_params['mask_path']), recon_params['voxel_size'], 'nearest')
-    nib.save(mask_downsampled_nii, filename=os.path.join(session_dir, "extra_data", "brain-mask.nii"))
-    del mask_downsampled_nii
-    print("Image-space cropping of segmentation...")
-    seg_downsampled_nii = qsm_forward.resize(nib.load(tissue_params['seg_path']), recon_params['voxel_size'], 'nearest')
-    nib.save(seg_downsampled_nii, filename=os.path.join(session_dir, "extra_data", "brain-mask.nii"))
-    del seg_downsampled_nii
-
-    # signal model
-    multiecho = len(recon_params['TEs']) > 1
-    for i in range(len(recon_params['TEs'])):
-        print(f"Computing MR signal for echo {i+1}...")
-        recon_name_i = f"{recon_name}_echo-{i+1}" if multiecho else recon_name
-        sigHR = qsm_forward.generate_signal(
-            field=field,
-            B0=recon_params['B0'],
-            TR=recon_params['TR'],
-            TE=recon_params['TEs'][i],
-            flip_angle=recon_params['flip_angle'],
-            phase_offset=phase_offset,
-            R1=nib.load(tissue_params['R1_path']).get_fdata(),
-            R2star=nib.load(tissue_params['R2star_path']).get_fdata(),
-            M0=nib.load(tissue_params['M0_path']).get_fdata()
-        )
-        #nib.save(nib.Nifti1Image(dataobj=np.abs(sigHR), affine=chi_nii.affine, header=chi_nii.header), filename=os.path.join(recon_params['out_dir'], "mag.nii"))
-        #nib.save(nib.Nifti1Image(dataobj=np.angle(sigHR), affine=chi_nii.affine, header=chi_nii.header), filename=os.path.join(recon_params['out_dir'], "phs.nii"))
-    
-        # k-space cropping of sigHR
-        print(f"k-space cropping of MR signal for echo {i+1}...")
-        resolution = np.array(np.round((np.array(chi_nii.header.get_zooms()) / recon_params['voxel_size']) * np.array(chi_nii.header.get_data_shape())), dtype=int)
-        sigHR_cropped = qsm_forward.crop_kspace(sigHR, resolution)
-        del sigHR
-        #nib.save(nib.Nifti1Image(dataobj=np.abs(sigHR_cropped), affine=chi_cropped_nii.affine, header=chi_cropped_nii.header), filename=os.path.join(recon_params['out_dir'], "cropped_mag.nii"))
-        #nib.save(nib.Nifti1Image(dataobj=np.angle(sigHR_cropped), affine=chi_cropped_nii.affine, header=chi_cropped_nii.header), filename=os.path.join(recon_params['out_dir'], "cropped_phs.nii"))
-
-        # noise
-        print(f"Simulating noise for echo {i+1}...")
-        sigHR_cropped_noisy = qsm_forward.add_noise(sigHR_cropped, peak_snr=recon_params['peak_snr'])
-        del sigHR_cropped
-
-        # save nifti images
-        mag_filename = f"{recon_name_i}_part-mag" + ("_MEGRE" if multiecho else "_T2starw")
-        phs_filename = f"{recon_name_i}_part-phase" + ("_MEGRE" if multiecho else "_T2starw")
-        nib.save(nib.Nifti1Image(dataobj=np.abs(sigHR_cropped_noisy), affine=chi_downsampled_nii.affine, header=chi_downsampled_nii.header), filename=os.path.join(recon_params['out_dir'], f"{mag_filename}.nii"))
-        nib.save(nib.Nifti1Image(dataobj=np.angle(sigHR_cropped_noisy), affine=chi_downsampled_nii.affine, header=chi_downsampled_nii.header), filename=os.path.join(recon_params['out_dir'], f"{phs_filename}.nii"))
-
-        # json header
-        print(f"Creating JSON headers...")
-        json_dict = { 'EchoTime': recon_params['TEs'][i], 'MagneticFieldStrength': recon_params['B0'], 'EchoNumber': i+1, 'ProtocolName': 'T2starw', 'ConversionSoftware': 'qsm-forward' }
-
-        json_dict_phs = json_dict.copy()
-        json_dict_phs['ImageType'] = ['P', 'PHASE']
-        json_dict_mag = json_dict.copy()
-        json_dict_mag['ImageType'] = ['M', 'MAGNITUDE']
-
-        with open(os.path.join(recon_params['out_dir'], f"{mag_filename}.json"), 'w') as mag_json_file:
-            json.dump(json_dict_mag, mag_json_file)
-        with open(os.path.join(recon_params['out_dir'], f"{phs_filename}.json"), 'w') as phs_json_file:
-            json.dump(json_dict_phs, phs_json_file)
-
-    print("Done!")
+    qsm_forward.generate_bids(tissue_params=tissue_params, recon_params=recon_params, bids_dir=bids_dir)
 
--- qsm_forward/qsm_forward.py
+++ qsm_forward/qsm_forward.py
@@ -1,6 +1,158 @@
+"""
+Author: Ashley Stewart <a.stewart.au@gmail.com>
+
+"""
+
 import numpy as np
 from dipy.denoise.gibbs import gibbs_removal
 from nilearn.image import resample_img
+
+import json
+import os
+import nibabel as nib
+import numpy as np
+import qsm_forward
+
+# Default parameters for tissue
+default_tissue_params = {
+    "chi_path" : "ChiModelMIX_noCalc.nii.gz",
+    "M0_path" : "M0.nii.gz",
+    "R1_path" : "R1.nii.gz",
+    "R2star_path" : "R2star.nii.gz",
+    "mask_path" : "BrainMask.nii.gz",
+    "seg_path" : "SegmentedModel.nii.gz"
+}
+
+# Default parameters for reconstruction
+default_recon_params = {
+    "Subject" : "1",                # Subject ID
+    "Session" : "1",                # Session ID
+    "Run" : 1,                      # Run number
+    "TR" : 50e-3,                   # Repetition time (in seconds)
+    "TEs" : np.array([ 4e-3, 12e-3, 20e-3, 28e-3 ]), # Echo times (in seconds)
+    "flip_angle" : 15,              # Flip angle (in degrees)
+    "B0" : 7,                       # Magnetic field strength (in Tesla)
+    "B0_dir" : np.array([0, 0, 1]), # B0 field direction
+    "phase_offset" : 0,             # Phase offset (in radians)
+    "shimm" : False,                # Boolean to control shimming
+    "voxel_size" : np.array([1.0, 1.0, 1.0]), # Voxel size (in mm)
+    "peak_snr" : np.inf             # Peak signal-to-noise ratio
+}
+
+def generate_bids(tissue_params, recon_params, bids_dir):
+    """
+    Generate T2*-weighted MRI signals and save the outputs in the BIDS-compliant format.
+
+    This function simulates a T2*-weighted MRI signal based on a ground truth susceptibility map,
+    computes the necessary parameters, and saves the outputs (images, JSON headers) in the BIDS-compliant
+    format in the specified directory.
+
+    Parameters:
+    tissue_params (dict): A dictionary containing paths to different tissue parameter files.
+        Keys are: "chi_path", "M0_path", "R1_path", "R2star_path", "mask_path", and "seg_path".
+
+    recon_params (dict): A dictionary containing the parameters for the reconstruction simulation.
+        Keys are: "Subject", "Session", "Run", "TR", "TEs", "flip_angle", "B0", "B0_dir",
+        "phase_offset", "shimm", "voxel_size", "peak_snr".
+
+    bids_dir (str): The directory where the BIDS-formatted outputs will be saved.
+
+    Returns:
+    None. Outputs are saved as files in the bids_dir directory.
+
+    """
+
+    # create output directories
+    print("Creating output directory...")
+    os.makedirs(bids_dir, exist_ok=True)
+    
+    # recon name
+    recon_name = f"sub-{recon_params['Subject']}_ses-{recon_params['Session']}_run-{recon_params['Run']}"
+    session_dir = os.path.join(bids_dir, f"sub-{recon_params['Subject']}", f"ses-{recon_params['Session']}")
+    os.makedirs(os.path.join(session_dir, 'anat'), exist_ok=True)
+    os.makedirs(os.path.join(session_dir, 'extra_data'), exist_ok=True)
+
+    # image-space resizing
+    chi_nii = nib.load(tissue_params['chi_path'])
+    print("Image-space resizing of chi...")
+    chi_downsampled_nii = qsm_forward.resize(chi_nii, recon_params['voxel_size'])
+    nib.save(chi_downsampled_nii, filename=os.path.join(session_dir, "extra_data", "chi.nii"))
+    print("Image-space cropping of brain mask...")
+    mask_downsampled_nii = qsm_forward.resize(nib.load(tissue_params['mask_path']), recon_params['voxel_size'], 'nearest')
+    nib.save(mask_downsampled_nii, filename=os.path.join(session_dir, "extra_data", "brain-mask.nii"))
+    del mask_downsampled_nii
+    print("Image-space cropping of segmentation...")
+    seg_downsampled_nii = qsm_forward.resize(nib.load(tissue_params['seg_path']), recon_params['voxel_size'], 'nearest')
+    nib.save(seg_downsampled_nii, filename=os.path.join(session_dir, "extra_data", "segmentation.nii"))
+    del seg_downsampled_nii
+
+    # calculate field
+    print("Computing field model...")
+    chi = chi_nii.get_fdata()
+    field = qsm_forward.generate_field(chi)
+    del chi
+
+    # simulate shim field
+    print("Computing shim fields...")
+    brain_mask = nib.load(tissue_params['mask_path']).get_fdata()
+    _, field, _ = qsm_forward.generate_shimmed_field(field, brain_mask, order=2)
+
+    # phase offset
+    print("Computing phase offset...")
+    M0 = nib.load(tissue_params['M0_path']).get_fdata()
+    phase_offset = qsm_forward.generate_phase_offset(M0, brain_mask, M0.shape)
+    del brain_mask
+
+    # signal model
+    multiecho = len(recon_params['TEs']) > 1
+    for i in range(len(recon_params['TEs'])):
+        print(f"Computing MR signal for echo {i+1}...")
+        recon_name_i = f"{recon_name}_echo-{i+1}" if multiecho else recon_name
+        sigHR = qsm_forward.generate_signal(
+            field=field,
+            B0=recon_params['B0'],
+            TR=recon_params['TR'],
+            TE=recon_params['TEs'][i],
+            flip_angle=recon_params['flip_angle'],
+            phase_offset=phase_offset,
+            R1=nib.load(tissue_params['R1_path']).get_fdata(),
+            R2star=nib.load(tissue_params['R2star_path']).get_fdata(),
+            M0=nib.load(tissue_params['M0_path']).get_fdata()
+        )
+    
+        # k-space cropping of sigHR
+        print(f"k-space cropping of MR signal for echo {i+1}...")
+        resolution = np.array(np.round((np.array(chi_nii.header.get_zooms()) / recon_params['voxel_size']) * np.array(chi_nii.header.get_data_shape())), dtype=int)
+        sigHR_cropped = qsm_forward.crop_kspace(sigHR, resolution)
+        del sigHR
+
+        # noise
+        print(f"Simulating noise for echo {i+1}...")
+        sigHR_cropped_noisy = qsm_forward.add_noise(sigHR_cropped, peak_snr=recon_params['peak_snr'])
+        del sigHR_cropped
+
+        # save nifti images
+        mag_filename = f"{recon_name_i}_part-mag" + ("_MEGRE" if multiecho else "_T2starw")
+        phs_filename = f"{recon_name_i}_part-phase" + ("_MEGRE" if multiecho else "_T2starw")
+        nib.save(nib.Nifti1Image(dataobj=np.abs(sigHR_cropped_noisy), affine=chi_downsampled_nii.affine, header=chi_downsampled_nii.header), filename=os.path.join(session_dir, "anat", f"{mag_filename}.nii"))
+        nib.save(nib.Nifti1Image(dataobj=np.angle(sigHR_cropped_noisy), affine=chi_downsampled_nii.affine, header=chi_downsampled_nii.header), filename=os.path.join(session_dir, "anat", f"{phs_filename}.nii"))
+
+        # json header
+        print(f"Creating JSON headers...")
+        json_dict = { 'EchoTime': recon_params['TEs'][i], 'MagneticFieldStrength': recon_params['B0'], 'EchoNumber': i+1, 'ProtocolName': 'T2starw', 'ConversionSoftware': 'qsm-forward' }
+
+        json_dict_phs = json_dict.copy()
+        json_dict_phs['ImageType'] = ['P', 'PHASE']
+        json_dict_mag = json_dict.copy()
+        json_dict_mag['ImageType'] = ['M', 'MAGNITUDE']
+
+        with open(os.path.join(session_dir, "anat", f"{mag_filename}.json"), 'w') as mag_json_file:
+            json.dump(json_dict_mag, mag_json_file)
+        with open(os.path.join(session_dir, "anat", f"{phs_filename}.json"), 'w') as phs_json_file:
+            json.dump(json_dict_phs, phs_json_file)
+
+    print("Done!")
+
 
 def generate_field(chi):
     """
