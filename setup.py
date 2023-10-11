3a6c0d4fc8c2e8ae5f4e44130b4c5bf59a4cd1c1
Ashley Stewart <a.stewart.au@gmail.com>
2023-10-11 13:42:47 +1000

    v0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qsm-forward',
-    version='0.16',
+    version='0.17',
     packages=find_packages(),
     url='https://github.com/astewartau/qsm-forward',
     author='Ashley Stewart',
